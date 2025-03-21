ba4236cfd575e6f6104a4eb17da3e30c6e109264
developmentIntensified <developmentIntensified@gmail.com>
2025-03-21 15:40:15 -0400

    adding ftp push

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <link rel="stylesheet" href="./src/css/index.css" />
-
-<head />
-
-<body class="m-0">
-    <?php include './src/components/navbar.php'; ?>
-
-    <?php include './src/components/footer.php'; ?>
-<body />
-<html />
+<?php require_once 'src/components/layout.php'; ?>
--- src/components/navbar.php
+++ src/components/navbar.php
@@ -2,14 +2,14 @@
 <?php
 
 $links = array(
-    array("/week1foundations", "Module 1: Week 1 Foundations"),
-    array("/week1variables","Module 1: Week 1 Variables"),
-    array("/week2forms","Module 2: Week 2 Forms"),
-    array("/week3arrays","Module 3: Week 3 Arrays"),
-    array("/week4sessions","Module 4: Week 4 Sessions"),
-    array("/week5cmssessions","Module 5: Week 5 CMS Sessions"),
-    array("/week6database","Module 6: Week 6 Database"),
-    array("/week8cmsdatabase","Module 8: Week 8 CMS Database"),
+    array("./week1foundations.php", "Module 1: Week 1 Foundations"),
+    array("./week1variables.php","Module 1: Week 1 Variables"),
+    array("./week2forms.php","Module 2: Week 2 Forms"),
+    array("./week3arrays.php","Module 3: Week 3 Arrays"),
+    array("./week4sessions.php","Module 4: Week 4 Sessions"),
+    array("./week5cmssessions.php","Module 5: Week 5 CMS Sessions"),
+    array("./week6database.php","Module 6: Week 6 Database"),
+    array("./week8cmsdatabase.php","Module 8: Week 8 CMS Database"),
 );
     for ($link=0;$link<=count($links) - 1;$link += 1) { 
         echo "<a class='p-2 h-full text-black' href='".$links[$link][0]."'>".explode(":", $links[$link][1])[1]."</a>";
