d2bbf1a945506edac6250181ed6586a2add48137
developmentIntensified <developmentIntensified@gmail.com>
2025-04-07 00:33:39 -0400

    test

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ echo str_replace("/schoolproject", "", $_SERVER['REQUEST_URI']);
 echo $routes[str_replace("/schoolproject", "", $_SERVER['REQUEST_URI'])];
 
 if (array_key_exists(str_replace("/schoolproject", "", $_SERVER['REQUEST_URI']), $routes)) {
-    $pageContent = str_replace("/schoolproject", "", $_SERVER['REQUEST_URI']);
+    $pageContent = $routes[str_replace("/schoolproject", "", $_SERVER['REQUEST_URI'])];
+    echo $pageContent;
 };
 
 include "layout.php";
