65bfcfd9b676fffa080c99381058910f086d2bac
developmentIntensified <developmentIntensified@gmail.com>
2025-05-07 02:54:46 -0400

    fix

--- src/routes/employee.php
+++ src/routes/employee.php
@@ -1,9 +1,9 @@
 <?php
 include 'src/components/mysqlconnection.php';
 $id = $_GET['id'];
-
-if (isset($_GET["first-name"]) && isset($_SESSION["user"]) && $_SESSION["user"]["role"] == "admin" || $_SESSION["user"]["role"] == "publisher") {
-    $updateEmployeeSql = "update employees 
+if (isset($_SESSION["user"])) {
+    if (isset($_GET["first-name"]) && $_SESSION["user"]["role"] == "admin" || $_SESSION["user"]["role"] == "publisher") {
+        $updateEmployeeSql = "update employees 
     set firstName = '" . $_GET["first-name"] . "',
     lastName = '" . $_GET["last-name"] . "',
     jobTitle = '" . $_GET["job-title"] . "',
@@ -12,15 +12,16 @@ if (isset($_GET["first-name"]) && isset($_SESSION["user"]) && $_SESSION["user"][
     hobby = '" . $_GET["favorite-hobby"] . "'
     where id = " . $id;
 
-    if (mysqli_query($conn, $updateEmployeeSql)) {
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        if (mysqli_query($conn, $updateEmployeeSql)) {
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
     }
 }
 
 $sql = "SELECT * FROM employees where id = " . $id;
 $result = mysqli_query($conn, $sql);
-if(!$result) {
+if (!$result) {
     echo "<h1>Employee not found</h1>";
     return;
 }
@@ -39,9 +40,11 @@ echo "<h2>Favorite Book: " . $Employees[0]["favoritebook"] . "</h2>";
 echo "<h2>Hobby: " . $Employees[0]["hobby"] . "</h2>";
 echo "<img src='./src/images/" . $Employees[0]["image"] . "' alt='" . $Employees[0]["firstName"] . " " . $Employees[0]["lastName"] . "' width='300' height='400'>";
 
-if(isset($_SESSION["user"]) && $_SESSION["user"]["role"] == "admin") {
-    echo "<form action='./deleteemployee' method='post'>
+if (isset($_SESSION["user"])) {
+    if ($_SESSION["user"]["role"] == "admin") {
+        echo "<form action='./deleteemployee' method='post'>
     <input type='hidden' name='id' value='" . $Employees[0]["id"] . "'>
     <button class='m-2 p-2 rounded bg-primary-400 block' type='submit'>Delete Employee</button>
     </form>";
+    }
 }
